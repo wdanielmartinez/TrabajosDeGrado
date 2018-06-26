a5f6670eac730377fccf90270195788397d8df95
W Daniel <W Daniel@DESKTOP-NLV1RCE>
2018-06-26 15:18:43 -0600

    Implementación del driver JDBC y clase Conexion.java

--- src/Clases/Conexion.java
+++ src/Clases/Conexion.java
@@ -1,10 +1,36 @@
 package Clases;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class Conexion {
     
-    /*    
+    private final String base = "trabajodegrados";
+    private final String user = "root";
+    private final String password = "wild24174485***";//contraseña
+    private final String url = "jdbc://myql://localhost:3306/" + base;
+    private Connection con = null;
+    
     public Connection getConexion(){
         
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            try {
+                con = DriverManager.getConnection(url, user, password);
+            } catch (SQLException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   
+  
+        return con;
     }
-    */
+    
 }
